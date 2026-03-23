2d18fddd0abffa546b217146fe424da71c79e978
codenamezero6876 <121150779+codenamezero6876@users.noreply.github.com>
2026-03-23 20:07:00 +0800

    Update model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -1,4 +1,15 @@
-from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.classification import \
+    RandomForestClassifier, \
+    GBTClassifier, \
+    DecisionTreeClassifier, \
+    LinearSVC, \
+    NaiveBayes
+from pyspark.ml.tuning import \
+    ParamGridBuilder, \
+    CrossValidator
+from pyspark.ml.evaluation import \
+    BinaryClassificationEvaluator, \
+    MulticlassClassificationEvaluator
 from pyspark.sql import SparkSession, DataFrame
 
 import os
@@ -34,31 +45,88 @@ class ModelTrainer:
             return df
 
         except Exception as e:
-            logger.error(f"Error loading files: {str(e)}")
+            logger.error(f"Error loading files: {str(e)}", stacklevel=2)
             raise
 
-    def train_random_forest(self, train_data: DataFrame):
+    def train_gbt(self, train_data: DataFrame, eval_type="accuracy"):
+        """
+        Train GBT model with specified parameters.
+        """
+        try:
+            gbt = GBTClassifier(
+                labelCol="label",
+                featuresCol="features"
+            )
+
+            if eval_type == "None":
+                logger.info("Starting GBT training...")
+                model = gbt.fit(train_data)
+                logger.info("GBT training completed successfully")
+                return model
+            
+            param_grid = ParamGridBuilder() \
+                .addGrid(gbt.maxDepth, [5, 10, 20]) \
+                .addGrid(gbt.featureSubsetStrategy, ["auto", "sqrt", "log2"]) \
+                .addGrid(gbt.impurity, [True, False]) \
+                .build()
+            
+            if eval_type == "areaUnderROC" or eval_type == "areaUnderPR":
+                evaluator = BinaryClassificationEvaluator(metricName=eval_type)
+            else:
+                evaluator = MulticlassClassificationEvaluator(metricName=eval_type)
+
+            gbt_cv = CrossValidator(
+                estimator=gbt,
+                evaluator=evaluator,
+                estimatorParamMaps=param_grid
+            )
+
+            model = gbt_cv.fit(train_data).bestModel
+            return model
+
+        except Exception as e:
+            logger.error(f"Error training GBT model: {str(e)}", stacklevel=2)
+            raise
+
+    def train_random_forest(self, train_data: DataFrame, eval_type="accuracy"):
         """
         Train Random Forest model with specified parameters.
         """
         try:
             rf = RandomForestClassifier(
                 labelCol="label",
-                featuresCol="features",
-                numTrees=self.config.get("num_trees", 100),
-                maxDepth=self.config.get("max_depth", 10),
-                seed=self.config.get("seed", 42),
-                featureSubsetStrategy=self.config.get("fss", "auto"),
-                impurity=self.config.get("impurity", "gini")
+                featuresCol="features"
             )
 
-            logger.info("Starting Random Forest training...")
-            model = rf.fit(train_data)
-            logger.info("Random Forest training completed successfully")
-            return model
+            if eval_type == "None":
+                logger.info("Starting Random Forest training...")
+                model = rf.fit(train_data)
+                logger.info("Random Forest training completed successfully")
+                return model
 
+            param_grid = ParamGridBuilder() \
+                .addGrid(rf.numTrees, [20, 50, 100]) \
+                .addGrid(rf.maxDepth, [5, 10, 20]) \
+                .addGrid(rf.featureSubsetStrategy, ["auto", "sqrt", "log2"]) \
+                .addGrid(rf.impurity, [True, False]) \
+                .build()
+            
+            if eval_type == "areaUnderROC" or eval_type == "areaUnderPR":
+                evaluator = BinaryClassificationEvaluator(metricName=eval_type)
+            else:
+                evaluator = MulticlassClassificationEvaluator(metricName=eval_type)
+
+            rf_cv = CrossValidator(
+                estimator=rf,
+                evaluator=evaluator,
+                estimatorParamMaps=param_grid
+            )
+
+            fitted_model = rf_cv.fit(train_data)
+            return fitted_model
+            
         except Exception as e:
-            logger.error(f"Error training Random Forest Model: {str(e)}")
+            logger.error(f"Error training Random Forest Model: {str(e)}", stacklevel=2)
             raise
 
     def save_model(self, model, models_path: str):
@@ -71,7 +139,7 @@ class ModelTrainer:
             logger.info(f"Model saved successfully to {model_path}")
 
         except Exception as e:
-            logger.error(f"Error saving trained model: {str(e)}")
+            logger.error(f"Error saving trained model: {str(e)}", stacklevel=2)
             raise
 
 
@@ -99,7 +167,7 @@ def main():
         logger.info("Model training pipeline completed successfully")
 
     except Exception as e:
-        logger.error(f"Model training pipeline failed: {str(e)}")
+        logger.error(f"Model training pipeline failed: {str(e)}", stacklevel=2)
         raise
 
     finally:
